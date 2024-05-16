24d8f7e5dc4e34a03c69adb9133717fbcd36acb1
Marleen <49445100+aMiyukii@users.noreply.github.com>
2024-05-16 13:26:43 +0200

    Controller fix category

--- BookApp/Controllers/AddCategoryController.cs
+++ BookApp/Controllers/AddCategoryController.cs
@@ -1,53 +1,65 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using BookApp.Core.Interfaces;
-using BookApp.Models;
-using System.Threading.Tasks;
 using BookApp.Core.DTO;
 using BookApp.Core.Services;
+using BookApp.Models;
 
-    namespace BookApp.Controllers
+namespace BookApp.Controllers
+{
+    public class AddCategoryController : Controller
     {
-        public class AddCategoryController : Controller
+        private readonly CategoryServices _categoryServices;
+
+        public AddCategoryController(CategoryServices categoryServices)
         {
-            private readonly CategoryServices _categoryServices;
-
-            public AddCategoryController(CategoryServices categoryServices)
-            {
-                _categoryServices = categoryServices;
-            }
-
-            [HttpGet("/addcategory")]
-            public async Task<IActionResult> Index()
-            {
-                var categoriesDTO = await _categoryServices.GetAllCategoriesAsync();
-
-                var viewModel = new AddCategoryViewModel
-                {
-                    Categories = categoriesDTO
-                };
-
-                return View(viewModel);
-            }
-
-            [HttpPost("/AddCategory/AddCategoryName")]
-            public async Task<ActionResult> AddCategoryName(CategoryDTO category)
-            {
-                await _categoryServices.AddCategoryAsync(category);
-                return RedirectToAction("Index");
-            }
-
-            [HttpPost("/AddCategory/UpdateCategoryName")]
-            public async Task<ActionResult> UpdateCategoryName(CategoryDTO category)
-            {
-                await _categoryServices.UpdateCategoryAsync(category);
-                return RedirectToAction("Index");
-            }
-
-            [HttpPost("/AddCategory/DeleteCategory")]
-            public async Task<ActionResult> DeleteCategory(int id)
-            {
-                await _categoryServices.DeleteCategoryAsync(id);
-                return RedirectToAction("Index");
-            }
+            _categoryServices = categoryServices;
         }
+
+        [HttpGet("/addcategory")]
+        public async Task<IActionResult> Index()
+        {
+            var categoriesDto = await _categoryServices.GetAllCategoriesAsync();
+            var viewModel = new AddCategoryViewModel
+            {
+                Categories = categoriesDto
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost("/AddCategory/AddCategoryName")]
+        public async Task<ActionResult> AddCategoryName(CategoryDTO category)
+        {
+            if (string.IsNullOrEmpty(category.Name))
+            {
+                Console.WriteLine("Error: Empty category name");
+                return RedirectToAction("Index");
+            }
+
+            await _categoryServices.AddCategoryAsync(category);
+            return RedirectToAction("Index");
+        }
+        
+        [HttpPost("/AddCategory/UpdateCategoryName")]
+        public async Task<ActionResult> UpdateCategoryName(int id, string newName)
+        {
+            if (string.IsNullOrEmpty(newName))
+            {
+                Console.WriteLine("Error: Empty category name");
+                return RedirectToAction("Index");
+            }
+            var category = await _categoryServices.GetCategoryByIdAsync(id);
+            category.Name = newName;
+
+            await _categoryServices.UpdateCategoryAsync(category);
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpPost("/AddCategory/DeleteCategory")]
+        public async Task<ActionResult> DeleteCategory(int id)
+        {
+            await _categoryServices.DeleteCategoryAsync(id);
+            return RedirectToAction("Index");
+        }
+    }
 }
