30795f061e0546e250061aa7f02f73bca57c5b53
Marleen <49445100+aMiyukii@users.noreply.github.com>
2024-05-22 10:43:06 +0200

    Install Xunit

---
+++ BookApp.Unittest/AddBookTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+namespace BookApp.Unittest;
+
+public class AddBookTest
+{
+    [Fact]
+    public void TestGetBook()
+    {
+        //Arange
+        //Act
+        //Assert
+    }
+}
