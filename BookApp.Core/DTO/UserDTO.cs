dbdaca0a2d066ce2a1d7f7127531c27aca258c4c
Marleen <marleeneijkemans@gmail.com>
2024-06-12 16:34:08 +0200

    Update userDTO

--- BookApp.Core/DTO/UserDTO.cs
+++ BookApp.Core/DTO/UserDTO.cs
@@ -2,5 +2,20 @@ namespace BookApp.Core.DTO;
 
 public class UserDTO
 {
+    private int Id { get; set; }
+    private string Emailaddress { get; set; }
+    private string Password { get; set; }
+
+    public UserDTO()
+    {
+        
+    }
+    public UserDTO(int id, string emailaddress, string password)
+    {
+        Id = id;
+        Emailaddress = emailaddress;
+        Password = password;
+    }
+    
     
 }
